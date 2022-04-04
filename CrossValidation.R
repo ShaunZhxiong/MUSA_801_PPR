f5b125e5a37500a7eedec97e31c474e5deefb924
Hui-Tian <htian.la@gmail.com>
2022-04-04 12:57:22 -0400

    UpdateOutputData

--- CrossValidation.R
+++ CrossValidation.R
@@ -28,4 +28,59 @@ library(rgdal)
 
 setwd("C:\\PENN\\2022\\MUSA_801_Practicum\\MUSA_801_PPR\\data\\output")
 
-modelPlaces <- st_read('modelPlaces.GEOJSON')
+# Read local datasets
+
+#modelPlaces <- st_read('modelPlaces.GEOJSON')
+modelData <- read_csv('data/output/SampleModelData.csv')
+
+# count one origin respond to the number of parks
+x <- modelData %>% group_by(origin) %>% count()
+
+# sample orgin
+
+origin = "421019891001"
+
+sample = subset(modelData, origin == "421019891001")
+
+# Define Function
+
+crossValidate <- function(sample, id, probability, indVariables) {
+  
+  allPredictions <- data.frame()
+  cvID_list <- unique(sample[[id]])
+  
+  for (i in cvID_list) {
+    
+    thisFold <- i
+    cat("This hold out fold is", thisFold, "\n")
+    
+    fold.train <- filter(sample, sample[[id]] != thisFold) %>% as.data.frame() %>% 
+      dplyr::select(id, geometry, indVariables, dependentVariable)
+    fold.test  <- filter(sample, sample[[id]] == thisFold) %>% as.data.frame() %>% 
+      dplyr::select(id, geometry, indVariables, dependentVariable)
+   
+    regression <-
+    lm(y ~ ., data=sample %>% 
+         dplyr::select(-origin, -id))
+    
+    thisPrediction <- 
+      mutate(fold.test, Prediction = predict(regression, fold.test, type = "response"))
+    
+    allPredictions <-
+      rbind(allPredictions, thisPrediction)
+    
+  }
+  return(st_sf(allPredictions))
+}
+
+reg.vars <- c( "visitors", "attr", "distance", "centrality")
+
+###  create spatial cross validation
+reg.spatialCV <- crossValidate(
+  dataset = sample,
+  id = "id",                            
+  dependentVariable = "prob",
+  indVariables = reg.vars) 
+#  dplyr::select(cvID = id, visitor, prob)
+
+
